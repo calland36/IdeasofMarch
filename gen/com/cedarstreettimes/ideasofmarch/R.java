c3c1eddc6e2d1c24ef0c8704b31621633b49590e
Keith Gudger <kgudger@gmail.com>
2013-03-16 12:35:44 -0700

    Added logo banner to main page.

--- gen/com/cedarstreettimes/ideasofmarch/R.java
+++ gen/com/cedarstreettimes/ideasofmarch/R.java
@@ -174,31 +174,33 @@ or to a theme attribute in the form "<code>?[<i>package</i>:][<i>type</i>:]<i>na
         public static final int indicator_bg_top=0x7f02000c;
         public static final int list_selector=0x7f02000d;
         public static final int logo=0x7f02000e;
+        public static final int treeslogo=0x7f02000f;
     }
     public static final class id {
         public static final int both=0x7f050003;
         public static final int disabled=0x7f050000;
         public static final int flip=0x7f050008;
         public static final int gridview=0x7f050009;
-        public static final int imageViewArticleLeft=0x7f050011;
-        public static final int imageViewArticleRight=0x7f050013;
-        public static final int listViewCopLog=0x7f05000f;
-        public static final int listViewEvents=0x7f050010;
-        public static final int listViewNews=0x7f05000d;
-        public static final int listViewUpdates=0x7f05000e;
+        public static final int imageView1=0x7f05000d;
+        public static final int imageViewArticleLeft=0x7f050012;
+        public static final int imageViewArticleRight=0x7f050014;
+        public static final int listViewCopLog=0x7f050010;
+        public static final int listViewEvents=0x7f050011;
+        public static final int listViewNews=0x7f05000e;
+        public static final int listViewUpdates=0x7f05000f;
         public static final int manualOnly=0x7f050004;
         public static final int pullDownFromTop=0x7f050005;
         public static final int pullFromEnd=0x7f050002;
         public static final int pullFromStart=0x7f050001;
         public static final int pullUpFromBottom=0x7f050006;
-        public static final int pull_to_refresh_image=0x7f050014;
-        public static final int pull_to_refresh_progress=0x7f050015;
-        public static final int pull_to_refresh_sub_text=0x7f050017;
-        public static final int pull_to_refresh_text=0x7f050016;
+        public static final int pull_to_refresh_image=0x7f050015;
+        public static final int pull_to_refresh_progress=0x7f050016;
+        public static final int pull_to_refresh_sub_text=0x7f050018;
+        public static final int pull_to_refresh_text=0x7f050017;
         public static final int rotate=0x7f050007;
         public static final int scrollview=0x7f05000b;
         public static final int tabhost=0x7f05000c;
-        public static final int textViewArticle=0x7f050012;
+        public static final int textViewArticle=0x7f050013;
         public static final int webview=0x7f05000a;
     }
     public static final class layout {
